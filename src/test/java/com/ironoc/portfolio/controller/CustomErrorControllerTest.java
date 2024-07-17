346d3f02cab740cf53d304b268ee8e0d43935cf0
Conor H <conor.heffron@gmail.com>
2024-07-17 15:33:06 +0100

    Adding unit tests for controllers

---
+++ src/test/java/com/ironoc/portfolio/controller/CustomErrorControllerTest.java
@@ -0,0 +1,46 @@
+package com.ironoc.portfolio.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomErrorControllerTest {
+
+    @InjectMocks
+    private CustomErrorController customErrorController;
+
+    @Mock
+    private HttpServletRequest httpServletRequestMock;
+
+    @Mock
+    private HttpServletResponse httpServletResponseMock;
+
+    @Test
+    public void test_error_view_success() {
+        // when
+        RedirectView result = customErrorController.error(httpServletRequestMock, httpServletResponseMock);
+
+        // then
+        assertThat(result, is(notNullValue()));
+        assertThat(result, isA(RedirectView.class));
+    }
+
+    @Test
+    public void test_getErrorPath_success() {
+        // when
+        String result = customErrorController.getErrorPath();
+
+        // then
+        assertThat(result, is("/error"));
+    }
+}
