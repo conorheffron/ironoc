cefd1614f8bca545c456b3f2b8556fef86832972
Conor Heffron <conor.heffron@gmail.com>
2024-07-23 17:34:29 +0100

    Update pom.xml (#39)
    * Update pom.xml
    
    tidy up spring boot dependencies
    
    * Resolve spring test conflict
    
    Bug fix - tests were not running as part of build after upgrading spring dependencies

--- src/test/java/com/ironoc/portfolio/AppTest.java
+++ src/test/java/com/ironoc/portfolio/AppTest.java
@@ -1,15 +1,12 @@
 package com.ironoc.portfolio;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.SpringApplication;
 
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 
-@RunWith(MockitoJUnitRunner.class)
 public class AppTest {
 
     // mocks
--- src/test/java/com/ironoc/portfolio/controller/HomeControllerTest.java
+++ src/test/java/com/ironoc/portfolio/controller/HomeControllerTest.java
@@ -1,18 +1,18 @@
 package com.ironoc.portfolio.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class HomeControllerTest {
 
     @InjectMocks
