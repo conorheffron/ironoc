a0d240184c5106318383d8f19c8d11b61f09d67c
Conor H <conor.heffron@gmail.com>
2024-08-27 13:29:56 +0100

    Migrate logging to use SLF4J annotations

--- src/main/java/com/ironoc/portfolio/controller/CustomErrorController.java
+++ src/main/java/com/ironoc/portfolio/controller/CustomErrorController.java
@@ -3,24 +3,21 @@ package com.ironoc.portfolio.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@Slf4j
 public class CustomErrorController implements ErrorController {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(CustomErrorController.class);
-	
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
     public RedirectView error(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-    	LOGGER.error("Bad request - redirect to home");
+    	log.error("Bad request - redirect to home");
         return new RedirectView("/", false);
     }
 
--- src/main/java/com/ironoc/portfolio/controller/HomeController.java
+++ src/main/java/com/ironoc/portfolio/controller/HomeController.java
@@ -2,19 +2,17 @@ package com.ironoc.portfolio.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
-	
-	private static Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@RequestMapping( {"/"} )
 	public String index(HttpServletRequest httpServletRequest) {
-		LOGGER.info(httpServletRequest.getRequestURI() + " page request");
+		log.info(httpServletRequest.getRequestURI() + " page request");
 		return "index";
 	}
 }
