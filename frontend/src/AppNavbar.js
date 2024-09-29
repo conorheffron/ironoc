7b942cb2cce54c9b0ab13715915a6dffdc80b697
Conor H <conor.heffron@gmail.com>
2024-09-29 01:37:47 +0100

    #62 Update AppNavbar.js

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -1,26 +1,39 @@
-import React, {Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Navbar, Container, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownItem, DropdownMenu } from "reactstrap";
 
-export default class AppNavbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isOpen: false};
-        this.toggle = this.toggle.bind(this);
-    }
-
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
-    render() {
-        return <Navbar color="dark" dark expand="md">
-            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
-            <NavbarBrand tag={Link} to="/about">About</NavbarBrand>
-            <NavbarBrand tag={Link} to="/projects">GitHub Projects</NavbarBrand>
-            <NavbarBrand tag={Link} to="/swagger-ui-ironoc.html" target="_blank">iRonoc API</NavbarBrand>
-        </Navbar>;
-    }
+export default function AppNavBar() {
+  return (
+    <div>
+      <Navbar color="light" fixed="top" light expand={true} className="nav-bar">
+      <Container>
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/about/">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/projects">GitHub Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink target="_blank" href="/swagger-ui-ironoc.html">iRonoc API</NavLink>
+            </NavItem>
+            <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                API Options
+              </DropdownToggle>
+              <DropdownMenu end>
+                <DropdownItem href="/get-repo-detail?username=conorheffron">Projects JSON</DropdownItem>
+                <DropdownItem>Issues JSON</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem href="/">Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
