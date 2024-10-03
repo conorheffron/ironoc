0260f70e4aad0d696ba43f76ec67eaa37c44ef53
Conor H <conor.heffron@gmail.com>
2024-10-03 04:12:29 +0100

    #62 Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
 import logo from './robot-logo.png';
+import './App.css';
 
 class Home extends Component {
 
-  render() {
-    return (
-        <div className="App">
-          <AppNavbar/>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-          </header>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+              <AppNavbar/>
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+              </header>
+            </div>
+        );
+    }
 }
 
 export default Home;
