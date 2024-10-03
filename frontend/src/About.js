7865602143400bf903d4494adf0f5b5994043254
Conor H <conor.heffron@gmail.com>
2024-10-03 04:11:39 +0100

    #62 Update About.js

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
 
 class About extends Component {
 
@@ -19,17 +17,16 @@ class About extends Component {
               expanding my technical expertise across various tech stacks, languages, frameworks, and
               tools in the realm of Software, Data, and DevOps. Let's connect and explore exciting
               opportunities together! See below for contact details.</h4>
-              <a class="btn btn-info" role="button" href="https://linktr.ee/conorheffron" target="_blank">Link Tree</a>
+              <a class="btn btn-info" role="button" href="https://linktr.ee/conorheffron" target="_blank" rel="noreferrer">Link Tree</a>
               <br/> <br/>
-              <a href="https://www.linkedin.com/in/conorheffron"
-                 target="_blank">
+              <a href="https://www.linkedin.com/in/conorheffron" target="_blank" rel="noreferrer">
                  <img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x33.png" width="160" height="33" border="0"
                       alt="View Conor Heffron's profile on LinkedIn"></img>
                </a>
                <br/> <br/>
-               <iframe height='160' width='300' frameborder='0'
-                                   allowtransparency='true' scrolling='no'
-                                   src='https://www.strava.com/athletes/2582329/activity-summary/a106933e6b6e42ffb4dcf37d2b5fe047af61329a'></iframe>
+               <iframe height='160' width='300' frameborder='0' allowtransparency='true' scrolling='no' title='strava-badge'
+                        src='https://www.strava.com/athletes/2582329/activity-summary/a106933e6b6e42ffb4dcf37d2b5fe047af61329a'>
+               </iframe>
               </div>
           </header>
         </div>
