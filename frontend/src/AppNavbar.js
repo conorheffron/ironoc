de4b49c4aafd031f1d5c0f3cc3bbc2e445313b9a
Conor H <conor.heffron@gmail.com>
2024-10-05 00:51:57 +0100

    #71 Refactor AppNavbar.js

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -4,42 +4,43 @@ import {Navbar, Container, Collapse, NavbarBrand, Nav, NavItem, NavLink, Uncontr
 
 export default function AppNavBar() {
   return (
-    <div>
-      <Navbar color="light" fixed="top" light expand={true} className="nav-bar">
-      <Container>
-        <NavbarBrand href="/">Home</NavbarBrand>
-        <Collapse navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/about">About</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/highs">Highlights</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/projects">GitHub Projects</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/issues/ironoc">iRonoc Issues</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink target="_blank" href="/swagger-ui-ironoc.html">iRonoc API</NavLink>
-            </NavItem>
-            <UncontrolledDropdown inNavbar nav>
-              <DropdownToggle caret nav>
-                API Options
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem target="_blank" href="/get-repo-detail?username=conorheffron">Projects JSON</DropdownItem>
-                <DropdownItem target="_blank" href="/get-repo-issue/conorheffron/ironoc/">Issues JSON (iRonoc)</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem href="/">Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-        </Container>
-      </Navbar>
-    </div>
+        <div>
+            <Navbar color="light" fixed="top" light expand={true} className="nav-bar">
+                <Container>
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <Collapse navbar>
+                    <Nav className="me-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/highs">Portfolio</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/projects/conorheffron">Projects</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/issues/conorheffron/ironoc">Issues</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink target="_blank" href="/swagger-ui-ironoc.html">API Doc</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown inNavbar nav>
+                            <DropdownToggle caret nav>API Options</DropdownToggle>
+                            <DropdownMenu end>
+                                <DropdownItem target="_blank" href="/get-repo-detail?username=conorheffron">Projects JSON</DropdownItem>
+                                <DropdownItem target="_blank" href="/get-repo-issue/conorheffron/ironoc/">Issues JSON (iRonoc)</DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem href="/">Reset</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                         <NavItem>
+                             <NavLink href="https://linktr.ee/conorheffron" target="_blank" rel="noreferrer">Link Tree</NavLink>
+                         </NavItem>
+                    </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
   );
 }
