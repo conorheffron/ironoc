ef917d9d1d4ad6b9ccaaa8fddcadde0f13b5fa1c
Conor H <conor.heffron@gmail.com>
2024-10-05 00:51:20 +0100

    #71 Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import Home from './components/Home';
 
 test('renders learn react link', () => {
   render(<Home />);
