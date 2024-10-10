23a3700e9146b6be0c6a9478ed0422dbe0b32244
Conor Heffron <conor.heffron@gmail.com>
2024-10-10 14:15:06 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
