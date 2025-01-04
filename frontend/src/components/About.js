87020dff8ab78bf857d8c44de6900f595f92467c
Conor Heffron <conor.heffron@gmail.com>
2025-01-04 20:51:40 +0000

    Update About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -17,13 +17,14 @@ class About extends Component {
                                 width="160" height="33" border="0" alt="View Conor Heffron's profile on LinkedIn"></img>
                         </a>
                         <p id="my-intro"><br />Welcome, I'm Conor Heffron, a Software Engineer hailing from County Meath, Ireland.
-                        With over ten years of professional experience, I specialize in writing clean code and
+                        With over fourteen years of professional experience, I specialize in writing clean code and
                         developing high-performance applications. As a passionate Full Stack Developer, I am constantly
-                        expanding my technical expertise across various tech stacks, languages, frameworks, and
-                        tools in the realm of Software Engineering, Data Engineering, & DevOps.
+                        expanding my technical expertise across various tech stacks, languages, frameworks, and tools in the 
+                        realm of Software Engineering, Data Engineering, & DevOps.
                         <br /><br />
-                        I believe in continuous learning & practical skills that can be demonstrated in a positive & collaborative manner (open source is great!). 
-                        When not learning or working, I like jogging/cycling, music, cooking, & searching for new forms of caffeine & salsa verde!
+                        I believe in continuous learning & practical skills that can be demonstrated in a positive & collaborative 
+                        manner (open source is great!). When not learning or working, I like jogging/cycling, music, cooking, 
+                        pretending to be a coffee connoisseur, & searching for new forms of salsa verde / green sauce! 
                         <br /><br />
                         Let's connect and explore exciting
                         opportunities together! See above & beyond for contact details and further information.
