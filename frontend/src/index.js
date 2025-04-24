b9af76e3f52dee7157a338e29a1f4e2dc6a8112d
Conor Heffron <conor.heffron@gmail.com>
2025-04-25 00:54:18 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -7,9 +7,9 @@ import App from './App';
 import AppNavBar from './AppNavbar';
 import Footer from './Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <div className="app-wrapper">
         <AppNavBar />
         <div className="content-inner">
@@ -17,7 +17,7 @@ root.render(
         </div>
         <Footer />
     </div>
-  </React.StrictMode>
+  </StrictMode>
 );
 
 reportWebVitals();
