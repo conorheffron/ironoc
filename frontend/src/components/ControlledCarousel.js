1b296e92675dc3c7f925e58d20c08283e944b8e0
Conor H <conor.heffron@gmail.com>
2025-05-12 23:08:42 +0100

    #274 Remove hard-coded portfolio project items from React JS code & hit server-side end-point instead for the same data

--- frontend/src/components/ControlledCarousel.js
+++ frontend/src/components/ControlledCarousel.js
@@ -7,79 +7,60 @@ import AppNavbar from '.././AppNavbar';
 import navy from '.././img/darkblue-bg.png';
 import teal from '.././img/teal-bg.png';
 import red from '.././img/red-bg.png';
+import LoadingSpinner from '.././LoadingSpinner';
 
 class ControlledCarousel extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+            portfolioItems: [],
+            loading: true
+        };
+    }
+
+    async componentDidMount() {
+        const response = await fetch("/api/portfolio-items");
+        const body = await response.json();
+        this.setState({ portfolioItems: body, loading: false });
+    }
+
     render() {
-        const {
-            items = [
-                {
-                    link: "https://github.com/conorheffron/ironoc-db",
-                    img: navy,
-                    alt: "navy1",
-                    title: "ironoc-db",
-                    description: "Sample Data Manager Service with UI",
-                    techStack: "Java & Spring Boot, Thymeleaf Templating Engine, & MySQL.",
-                },
-                {
-                    link: "https://github.com/conorheffron/booking-sys",
-                    img: teal,
-                    alt: "teal2",
-                    title: "booking-sys",
-                    description: "Sample Reservations & Viewer System",
-                    techStack: "Python & Django Web App, JavaScript, SQLite3 or MySQL database.",
-                },
-                {
-                    link: "https://github.com/conorheffron/nba-stats",
-                    img: navy,
-                    alt: "navy3",
-                    title: "nba-stats",
-                    description: "NBA Analytics (Seasons 2015 - 2024): Player Statistics",
-                    techStack: "Jupyter Notebooks, Python, Pandas, PandasAI, OpenAI GPT-3.5 Turbo, LLM, & Requests / JSON API.",
-                },
-                {
-                    link: "https://github.com/conorheffron/cbio-skin-canc",
-                    img: red,
-                    alt: "red4",
-                    title: "cbio-skin-canc",
-                    description: "Skin Cancer Dataset Analysis",
-                    techStack: "R, dplyr, plotly, knitr, testthat, covr, GIT.",
-                },
-                {
-                    link: "https://github.com/conorheffron/gene-expr",
-                    img: navy,
-                    alt: "navy5",
-                    title: "gene-expr",
-                    description: "Breast Cancer Dataset Analysis",
-                    techStack: "R, ggplot2, dplyr, deseq2-analysis, & R markdown.",
-                },
-                {
-                    link: "https://github.com/conorheffron/bio-cell-red-edge",
-                    img: red,
-                    alt: "red6",
-                    title: "bio-cell-red-edge",
-                    description: "Edge Detection of Biological Cell (Image Processing Script)",
-                    techStack: "Python, sci-kit-image, matplotlib.pyplot, & scipy.ndimage.",
-                },
-                {
-                    link: "https://github.com/conorheffron/global-max-sim-matrix",
-                    img: teal,
-                    alt: "teal7",
-                    title: "global-max-sim-matrix",
-                    description: "Compute Global Maximum Similarity Matrix",
-                    techStack: "R Package, testthat, stringr, & devtools.",
-                },
-            ]
-        } = this.props;
+        const { portfolioItems, loading} = this.state;
+
+        if (loading) {
+            return (
+                <div className="App">
+                    <AppNavbar />
+                    <Container>
+                        <br /><br /><br />
+                        <LoadingSpinner />
+                    </Container>
+                </div>
+            );
+        }
+
+        const handleColor = (color) => {
+            switch (color) {
+                case "red":
+                    return red;
+                case "teal":
+                    return teal;
+                case "navy":
+                    return navy;
+                default:
+                    return red;
+                    }
+            };
 
         return (
             <div>
                 <AppNavbar />
                 <Container>
                     <Carousel className="App-header">
-                        {items.map((item, index) => (
+                        {portfolioItems.map((item, index) => (
                             <Carousel.Item key={index} interval={500}>
                                 <a href={item.link} target="_blank" rel="noreferrer">
-                                    <img className="d-block w-100" src={item.img} alt={item.alt} />
+                                    <img className="d-block w-100" src={handleColor(item.img)} alt={item.alt} />
                                     <Carousel.Caption>
                                         <h1><u>{item.title}</u></h1>
                                         <h2>{item.description}</h2>
