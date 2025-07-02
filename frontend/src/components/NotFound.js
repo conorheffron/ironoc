0ec7fa88bedbedfba15cbfdf9a8e8353f076c010
Conor Heffron <conor.heffron@gmail.com>
2025-07-02 01:35:37 +0100

    Update NotFound.js

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import '../App.css';
-import AppNavbar from '../AppNavbar';
+import '.././App.css';
+import AppNavbar from '.././AppNavbar';
 
 function NotFound({ 
   title = "404 - Page Not Found", 
