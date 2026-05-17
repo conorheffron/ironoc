b00f48b359cf9e0bf7d362e7db9730ce40fb4925
Conor H <conor.heffron@gmail.com>
2026-05-17 23:46:19 +0100

    Rollback Apollo 4 changes

--- frontend/src/components/Donate.js
+++ frontend/src/components/Donate.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { gql } from '@apollo/client';
-import { useQuery } from '@apollo/client/react';
+import { withApollo } from '@apollo/client/react/hoc'; // HOC for injecting Apollo Client
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css';
@@ -26,33 +26,58 @@ export const GET_DONATE_ITEMS = gql`
     }
 `;
 
-function Donate() {
-    const { loading, error, data } = useQuery(GET_DONATE_ITEMS);
-    const donateItems = data?.donateItems || [];
+class Donate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            donateItems: [],
+            loading: true,
+            error: null,
+        };
+    }
 
-    if (loading) {
-        return (
-            <div className="App">
-                <AppNavbar />
-                <Container>
-                    <div className="mt-5">
+    async componentDidMount() {
+        const { client } = this.props; // Injected Apollo Client
+        try {
+            const { data } = await client.query({
+                query: GET_DONATE_ITEMS,
+            });
+            this.setState({ donateItems: data.donateItems, loading: false });
+        } catch (error) {
+            this.setState({ donateItems: [], error: error.message, loading: false });
+        }
+    }
+
+    render() {
+        const { donateItems, loading, error } = this.state;
+
+        if (loading) {
+            return (
+                <div className="App">
+                    <AppNavbar />
+                    <Container>
+                        <br />
+                        <br />
+                        <br />
                         <LoadingSpinner />
-                    </div>
-                </Container>
-            </div>
-        );
-    }
+                    </Container>
+                </div>
+            );
+        }
 
-    if (error) {
-        return (
-            <div className="App">
-                <AppNavbar />
-                <Container>
-                    <p className="mt-5">Error loading data: {error.message}</p>
-                </Container>
-            </div>
-        );
-    }
+        if (error) {
+            return (
+                <div className="App">
+                    <AppNavbar />
+                    <Container>
+                        <br />
+                        <br />
+                        <br />
+                        <p>Error loading data: {error}</p>
+                    </Container>
+                </div>
+            );
+        }
 
     return (
         <div className="App">
@@ -87,6 +112,38 @@ function Donate() {
             </Container>
         </div>
     );
+        return (
+            <div className="App">
+                <AppNavbar />
+                <Container>
+                    <Carousel className="App-header">
+                        {donateItems.map((item, index) => (
+                            <Carousel.Item key={index} interval={500}>
+                                <a href={item.donate} target="_blank" rel="noreferrer">
+                                    <img className="d-block w-100" src={red} alt={item.alt} />
+                                    <Carousel.Caption>
+                                        <h1>
+                                            <u>{item.name}</u>
+                                        </h1><br />
+                                        <h8>
+                                            <b>Contact & Help by Phone: </b><span dangerouslySetInnerHTML={{ __html: item.phone }} />
+                                        </h8><br />
+                                        <h7>
+                                            <b>Home page: </b><a href={item.link} target="_blank" rel="noreferrer">{item.link}</a>
+                                        </h7><br /><br />
+                                        <h11 className="overview-text">
+                                            <b>Overview:</b> Founded in {item.founded}, {item.overview}
+                                        </h11>
+                                    </Carousel.Caption>
+                                </a>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </Container>
+            </div>
+        );
+    }
 }
 
-export default Donate;
+// Wrap the component with Apollo Client
+export default withApollo(Donate);
