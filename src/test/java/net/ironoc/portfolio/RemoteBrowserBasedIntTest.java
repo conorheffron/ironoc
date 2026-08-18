ab68eecbdbc660a4fa082e89ec430c74c2fa81da
Conor Heffron <conor.heffron@gmail.com>
2026-08-18 15:58:53 +0100

    Adjust wait time in RemoteBrowserBasedIntTest
    Increased initial wait time for page loading in tests.

--- src/test/java/net/ironoc/portfolio/RemoteBrowserBasedIntTest.java
+++ src/test/java/net/ironoc/portfolio/RemoteBrowserBasedIntTest.java
@@ -47,7 +47,7 @@ class RemoteBrowserBasedIntTest extends BaseControllerIntegrationTest {
             webDriver.get("https://ironoc.net/");
 
             HomePage homePage = PageFactory.initElements(webDriver, HomePage.class);
-            getPageDetails(webDriver, 2);
+            getPageDetails(webDriver, 5);// increase intial wait time to 5 secs
             assertThat(homePage, is(notNullValue()));
 
             DonatePage donatePage = homePage.goToDonate();
