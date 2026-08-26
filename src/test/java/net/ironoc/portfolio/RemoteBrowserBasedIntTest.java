a4766f670d0d11b821cac426a64d00ac061fd258
Conor H <conor.heffron@gmail.com>
2026-08-26 20:17:43 +0100

    Update src/test/java/net/ironoc/portfolio/RemoteBrowserBasedIntTest.java

--- src/test/java/net/ironoc/portfolio/RemoteBrowserBasedIntTest.java
+++ src/test/java/net/ironoc/portfolio/RemoteBrowserBasedIntTest.java
@@ -47,7 +47,7 @@ class RemoteBrowserBasedIntTest extends BaseControllerIntegrationTest {
             webDriver.get("https://ironoc.net/");
 
             HomePage homePage = PageFactory.initElements(webDriver, HomePage.class);
-            getPageDetails(webDriver, 8);
+            getPageDetails(webDriver, 10);
             assertThat(homePage, is(notNullValue()));
 
             DonatePage donatePage = homePage.goToDonate();
